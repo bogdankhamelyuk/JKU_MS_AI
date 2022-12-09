09d87fe78d0d7e19fb5d6552df37163475de67da
Bogdan Khamelyuk <bogdan.khamelyuk@sie.at>
2022-12-09 05:46:03 +0100

    working on ex3 lstm a2

--- lstm&rnn/lstm_debug.py
+++ lstm&rnn/lstm_debug.py
@@ -22,19 +22,14 @@ class FullyRecurrentNetwork(object):
         self.x = x
         self.y = y
         self.a = np.zeros((T, I))
-
+        
         # iterate forward in time 
         # trick: access model.a[-1] in first iteration
         for t in range(T):
-            self.W @ x[t]
-            self.R @ self.a[t-1]
             self.a[t] = np.tanh(self.W @ x[t] + self.R @ self.a[t-1])
             
-        self.z = model.V @ self.a[t] 
-        loss = y * f(-self.z) + (1-y) * f(self.z)
-        return loss
-
-        
+        self.z = model.V @ self.a[t]
+        return y * f(-self.z) + (1-y) * f(self.z)
 
 T, D, I, K = 10, 3, 5, 1
 model = FullyRecurrentNetwork(D, I, K)
@@ -64,69 +59,60 @@ def generate_data(T):
     y2 = np.array([0.0],dtype=float)
 
     choice = np.random.choice([1,-1],1,p=[0.5,0.5])
-    if choice[0] == 1: 
-        return x1,y1# ,"positive"
+    if choice[0] == 1:
+        return x1,y1 #
          
     else:
-        return x2,y2# ,"negative"
+        return x2,y2 #
 
-np.random.seed(0xDEADBEEF)
-# comment this out and upper last categories to check what is distribution of "negative" and "positive" is
 
-# from collections import Counter
-# Counter(generate_data(5)[2] for i in range(10000))
-
-# x,y = generate_data(5)
-# print("x shape: ",x.shape)
-# print("x[0]:",x[0]," y[0]:",y[0])
-# print("y shape: ",y.shape)
 
 #EXERCISE 3
 
 def backward(self):
     
-    T = len(self.x)
-    psi = np.zeros(len(self.z),)
-    delta = np.zeros(T,)
+    delta = np.zeros((T,I))
     self.gradR = 0
     self.gradW = 0
     self.gradV = 0
-
+    dL_da = np.zeros((T,I))
+    
     # calculate psi[t]
-    frac = - self.z[-1]/( np.abs(self.z[-1]) * (1+np.exp(self.z[-1])) )
-    if self.z[-1] > 0:
-        frac += (1 - self.y)
-    psi[-1] = frac
+    # Loss formula was directly taken from forward pass 
+    psi = - (np.sign(self.z))/(np.exp(np.abs(self.z))+1) \
+          + (np.max([0,self.z])/self.z)*(1-self.y)
 
+    # just before start looping, calculate only one time 
+    # dL_da for the last position using psi, since we won't have psi[t]
+    dL_da[-1] = psi.reshape(1,1) @ self.V
+    
+    # calculate delta[t]:
     for t in reversed(range(0, T)):
-        # calculate s[t] 
-        s_t = self.W @ self.x[t]
-        if t > 0: # avoid out of range for self.a
-            s_t += self.R @ self.a[t-1]
-
-        # calculate delta[t]
-        if len(psi)!=1: 
-            dL_da = self.V.T @ [psi[t]]
-        if len(psi)==1 and t == (T-1):
-            dL_da = self.V.T @ [psi[-1]]
-        if t < (T - 1): # in case we aren't at the beginng and already have t+1 step ahead
-            self.R[:,:]*=delta[t+1]
-        da_ds = 1/(np.cosh(s_t))**2
         
-        delta[t] = dL_da @ da_ds
+        # 1) calculate dL/da[t]:
+        if t < (T-1):
+            dL_da[t] += delta[t+1] @ self.R 
+        
+        # 2) calculate da[t]/ds[t] 
+        s_t = self.W @ self.x[t].reshape(D,1)
+        if t > 0: # avoid out of range for self.a
+            s_t += self.R @ self.a[t-1].reshape(I,1)
+        da_ds = np.diag(1/(np.cosh(s_t))**2) 
+
+        delta[t] = dL_da[t].reshape(I,1) @ da_ds 
     
     # calculate gradients 
     for t in range(0, T):
         #calculate self.gradR
         if t!=0:       
-            self.gradR += delta[t] * self.a[t-1]
+            self.gradR += delta[t].reshape(I,1) @ self.a[t-1].reshape(1,I)
         # calculate gradW
-        self.gradW += delta[t] * self.x[t]
+        self.gradW += delta[t].reshape(I,1) @ self.x[t].reshape(1,D)
         # calculate self.gradV
         if len(psi)!=1:
-            self.gradV += psi[t] * self.a[t]
+            self.gradV += psi[t].reshape(K,K) @ self.a[t].reshape(K,I)
     if len(psi) == 1:
-        self.gradV += psi[-1]*self.a[-1]
+        self.gradV += psi[-1].reshape(K,K) @ self.a[-1].reshape(K,I)
     
 
 FullyRecurrentNetwork.backward = backward
@@ -151,24 +137,26 @@ def grad_check(self, eps, thresh):
     self.R = self.R - 2*self.eps
     # call forward() to make another forward pass
     lossMinusEps = self.forward(self.x,self.y)
-
+    
     # calculate numerical gradient
     gradApprox = (lossPlusEps - lossMinusEps)/(2*self.eps)
     
-    for dV in self.gradV:
-        diff = np.linalg.norm(dV-gradApprox)/(np.linalg.norm(dV)+np.linalg.norm(gradApprox))
-        if diff > self.thresh:
-            print("error")
-    for dW in self.gradW:
-        diff = np.linalg.norm(dW-gradApprox)/(np.linalg.norm(dW)+np.linalg.norm(gradApprox))
-        if diff > self.thresh:
-            print("error")
-    for dR in self.gradR:
-        diff = np.linalg.norm(dR-gradApprox)/(np.linalg.norm(dR)+np.linalg.norm(gradApprox))
-        if diff > self.thresh:
-            print("error")
+    for row in self.gradV:
+        for dV in row:
+            diff = np.linalg.norm(gradApprox-dV)/(np.linalg.norm(dV)+np.linalg.norm(gradApprox))
+            if diff > self.thresh:
+                print("error")
+    for row in self.gradW:
+        for dW in row:
+            diff = np.linalg.norm(-dW+gradApprox)/(np.linalg.norm(dW)+np.linalg.norm(gradApprox))
+            if diff > self.thresh:
+                print("schmerror")
+    for row in self.gradR:
+        for dR in row:
+            diff = np.linalg.norm(-dR+gradApprox)/(np.linalg.norm(dR)+np.linalg.norm(gradApprox))
+            if diff > self.thresh:
+                print("herror")
    
-    
     # restore model weights to the default ones
     self.W = self.W + self.eps
     self.V = self.V + self.eps
