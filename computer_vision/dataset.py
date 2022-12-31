49b51f860b5aeb5c5634579557d45a8c40e6fae7
Bogdan Khamelyuk <bogdan.khamelyuk@sie.at>
2022-12-31 22:18:03 +0600

    added video processing for the output

--- computer_vision/dataset.py
+++ computer_vision/dataset.py
@@ -14,7 +14,6 @@ class DroneThrmImg_Dataset(Dataset):
     # create arrays, where random and non-repeating numbers of videos in the working directory will be stored 
     train_file_names, test_file_names = [], [] 
     random.seed(10)
-    path = os.getcwd() + "/computer_vision/labels/"
 
     def __init__(self,req_samples_number, dataset_type):
         self.dataset_type = dataset_type
@@ -27,10 +26,10 @@ class DroneThrmImg_Dataset(Dataset):
  total number of available samples({self.total_json_number})")
         
         if self.dataset_type == 'TEST':
-            self.append_random_number(DroneThrmImg_Dataset.test_file_names,self.req_samples_number)
-            
+            self.random_videos = self.append_random_number(DroneThrmImg_Dataset.test_file_names,self.req_samples_number)
+
         elif self.dataset_type == 'TRAIN':
-            self.append_random_number(DroneThrmImg_Dataset.train_file_names,self.req_samples_number)
+            self.random_videos = self.append_random_number(DroneThrmImg_Dataset.train_file_names,self.req_samples_number)
 
         else:
             raise NameError(f"'{dataset_type}' is not in the list of available dataset types. \nAvailable types are: 'TEST' and 'TRAIN.")
@@ -40,20 +39,15 @@ class DroneThrmImg_Dataset(Dataset):
         return self.req_samples_number # return required number of samples, since that's exactly length of the each dataset
 
     def __getitem__(self, idx):
-        # ToDo: do it with instance list instead of if conditons
-        if self.dataset_type == 'TEST':
-            video_number = DroneThrmImg_Dataset.test_file_name[idx]
-        
-        if self.dataset_type == 'TRAIN':
-            video_number = DroneThrmImg_Dataset.train_file_name[idx]
-
-        video_number = DroneThrmImg_Dataset.test_file_name[idx] # take out random generated number of the video, according to its index
-        video_name = "video_" + str(video_number) + ".json" # append this number to "video_" and ".json", like all other videos are named
-        video_path = DroneThrmImg_Dataset.path+video_name # create complete path to the indexed video, i.e. "labels" of that video
-        labels = open(file=video_path, mode="r")
+        video_number = self.random_videos[idx] # take out random generated number of the video, according to its index
+        label_name = "video_" + str(video_number) + ".json" # append this number to "video_" and ".json", like all other videos are named
+        video_name = "video_" + str(video_number) + ".mp4"
+        label_path = path+label_name # create complete path to the indexed video, i.e. "labels" of that video
+        labels = open(file=label_path, mode="r")
         labels_json = json.load(labels)[str(video_number)] # adjust output of the json using [str(video_number)], to access labels
-        
-        video_labels = {"video_name":video_name, "labels":[]} # create return dictionary containing video name and labels for each frame
+        video_tensor = torchvision.io.read_video(path+video_name)[0] # create video tensor for the output
+        #print(video_tensor.shape)
+        output = {"video":video_tensor, "labels":[]} # create output return dictionary containing video name and labels for each frame
 
         for labeled_frame in labels_json: # each labeled frame in json has number of recogn. people and their coordinates, i.e. coordinates of label
             # create 16x16x3 grid, in each position of which is
@@ -66,9 +60,10 @@ class DroneThrmImg_Dataset(Dataset):
                 grid[int(row)][int(column)][0]=1 # probability that in this grid is person, make it to 100%
                 grid[int(row)][int(column)][1]=x # x-coordinate of the label
                 grid[int(row)][int(column)][2]=y # y-coordinate of the label
-            video_labels["labels"].append(grid) # append that 16x16x3 gridded frame to the labels
-        video_labels["labels"] = torch.stack(video_labels["labels"]) # convert labels to the torch-type tensor
-        return video_labels # ToDo: tuple containing 1)gelesenes video itself(look how2do using pytorch), 2)labeled frames & 3)augmented (use another class?)
+            output["labels"].append(grid) # append that 16x16x3 gridded frame to the labels
+        output["labels"] = torch.stack(output["labels"]) # convert labels to the torch-type tensor
+        #print(output["labels"].shape)
+        return output 
 
             
     def append_random_number(self, optional_list, samples_number):
@@ -81,17 +76,18 @@ class DroneThrmImg_Dataset(Dataset):
                         and not r in DroneThrmImg_Dataset.test_file_names:
                 optional_list.append(r)
                 i+=1
+        return optional_list
 
-         
 
 all_files = count_all_jsons()
 train_files = int(0.8*all_files)
 test_files = int(0.2*all_files)
 
-test_dataset = DroneThrmImg_Dataset(req_samples_number=train_files,  dataset_type='TEST')
-train_dataset = DroneThrmImg_Dataset(req_samples_number=test_files, dataset_type='TRAIN')
+test_dataset = DroneThrmImg_Dataset(req_samples_number=test_files,  dataset_type='TEST')
+train_dataset = DroneThrmImg_Dataset(req_samples_number=train_files, dataset_type='TRAIN')
 
 
 train_dataloader = DataLoader(train_dataset,shuffle=True)
 test_dataloader = DataLoader(test_dataset, shuffle=True)
 
+output = next(iter(train_dataloader))
